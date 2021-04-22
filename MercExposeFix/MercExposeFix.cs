b5da73841bd00de29b0142648b66b80ae8f74f03
Moffein <22742925+Moffein@users.noreply.github.com>
2021-04-22 05:57:22 -0700

    Reworked mod so that behavior is closer to Vanilla.

--- MercExposeFix/MercExposeFix.cs
+++ MercExposeFix/MercExposeFix.cs
@@ -7,65 +7,30 @@ using UnityEngine.Networking;
 
 namespace MercExposeFix
 {
-    [BepInPlugin("com.Moffein.MercExposeFix", "Merc Expose Fix", "1.0.1")]
+    [BepInPlugin("com.Moffein.MercExposeFix", "Merc Expose Fix", "1.1.0")]
     public class MercExposeFix : BaseUnityPlugin
     {
         public void Awake()
         {
             On.RoR2.HealthComponent.TakeDamage += (orig, self, damageInfo) =>
             {
-                bool applyExpose = damageInfo.attacker && (damageInfo.damageType & DamageType.ApplyMercExpose) > 0;
-                ExposeComponent ec = null;
-                GameObject oldExpose = null;
-                if (damageInfo.procCoefficient > 0f && self.body.HasBuff(RoR2Content.Buffs.MercExpose))
+                bool replaceExpose = false;
+                if (damageInfo.attacker && self.body.HasBuff(RoR2Content.Buffs.MercExpose))
                 {
-                    ec = self.gameObject.GetComponent<ExposeComponent>();
-                    if (ec && ec.pendingExpose == damageInfo.attacker)
+                    CharacterBody attackerBody = damageInfo.attacker.GetComponent<CharacterBody>();
+                    if (attackerBody && attackerBody.bodyIndex != BodyCatalog.FindBodyIndex("MercBody"))
                     {
-                        oldExpose = ec.pendingExpose;
-                        ec.pendingExpose = null;
+                        replaceExpose = true;
+                        attackerBody.RemoveBuff(RoR2Content.Buffs.MercExpose);
                     }
                 }
-
                 orig(self, damageInfo);
-
-                if (damageInfo.rejected && oldExpose)
+                if (replaceExpose)
                 {
-                    ec.pendingExpose = oldExpose;
-                }
-                else if (applyExpose && !damageInfo.rejected && self.alive)
-                {
-                    if (!ec)
-                    {
-                        ec = self.gameObject.AddComponent<ExposeComponent>();
-                    }
-                    ec.pendingExpose = damageInfo.attacker;
-                }
-            };
-
-            On.RoR2.CharacterBody.RemoveBuff_BuffIndex += (orig, self, buffIndex) =>
-            {
-                orig(self, buffIndex);
-                if (NetworkServer.active && buffIndex == RoR2Content.Buffs.MercExpose.buffIndex)
-                {
-                    ExposeComponent ec = self.gameObject.GetComponent<ExposeComponent>();
-                    if (ec && ec.pendingExpose)
-                    {
-                        CharacterBody cb = ec.pendingExpose.GetComponent<CharacterBody>();
-                        if (cb && cb.skillLocator)
-                        {
-                            cb.skillLocator.DeductCooldownFromAllSkillsServer(1f);
-                        }
-                        ec.pendingExpose = null;
-                    }
+                    self.body.AddBuff(RoR2Content.Buffs.MercExpose);
                 }
             };
         }
-    }
-
-    public class ExposeComponent : MonoBehaviour
-    {
-        public GameObject pendingExpose;
     }
 }
 
